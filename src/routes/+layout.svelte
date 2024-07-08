b14de4356f5f1ce5ac20861f603a5771b93b5bbc
matthew-townson <mjwtownson@gmail.com>
2024-07-08 18:26:11 +0100

    added frontend boilerplate
    stuff lifted from my website as a base, will change in future

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<nav>
+    <a href="#" id="title">Open Societyware</a>
+    <div class="navr">
+        <a href="#">Home</a>
+        <a href="#">Blog</a>
+        <a href="#">Login</a>
+    </div>
+</nav>
