69b06dfd2da8d0db38e541d9e58e8a89ea51ebfd
Matthew Townson <mjwtownson@gmail.com>
2024-07-13 18:44:18 +0100

    updated formatting

---
+++ notes/colours.md
@@ -0,0 +1,8 @@
+# Branding colours
+
+*Courtesy of coolors.co*  
+
+Logo: #A31621  
+Background: #FCF7F8  
+Navbar: #CED3DC  
+Misc colours: #4E8098, #90C2E7
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <nav>
-    <a href="#" id="title">Open Societyware</a>
+    <a href="/" id="title">Open Societyware</a>
     <div class="navr">
         <a href="#">Home</a>
         <a href="#">Blog</a>
         <a href="#">Login</a>
     </div>
 </nav>
+<div class="content"><slot></slot></div>
