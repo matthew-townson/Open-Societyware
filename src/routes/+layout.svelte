29e7d40b3d0c2a984c19df28f5180cf77471bdb7
matthew-townson <mjwtownson@gmail.com>
2024-07-15 15:38:45 +0100

    ul navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <nav>
-    <a href="/" id="title">Open Societyware</a>
-    <div class="navr">
-        <a href="#">Home</a>
-        <a href="#">Blog</a>
-        <a href="#">Login</a>
-    </div>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Contact</a></li>
+        <li style="float:left"><a class="title" href="/">Open Societyware</a></li>
+    </ul>
 </nav>
 <div class="content"><slot></slot></div>
