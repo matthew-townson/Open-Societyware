a6eaab44db889e942c86fc85c1b795ec95f1b8c5
matthew-townson <mjwtownson@gmail.com>
2024-08-06 22:49:53 +0100

    updated layout banner links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <nav>
     <a href="/" id="title">Open Societyware</a>
     <div class="navr">
-        <a href="#">Home</a>
-        <a href="#">Blog</a>
-        <a href="#">Login</a>
+        <a href="/">Home</a>
+        <a href="/blog">Blog</a>
+        <a href="/login">Login</a>
     </div>
 </nav>
 <div class="content"><slot></slot></div>
