6c84e72a62e0ae03b31885674daab56513d30041
Matthew Townson <mjwtownson@gmail.com>
2024-09-07 19:27:02 +0100

    added working db queries

--- src/lib/db.js
+++ src/lib/db.js
@@ -5,15 +5,20 @@ const dbPath = path.resolve('db/db.sqlite3');
 
 const db = new Database(dbPath);
 
-// checks if a user exists
-export const checkUser = (username) => {
+// get userID
+export const getUserID = (username) => {
     const query = db.prepare('SELECT userID FROM Users WHERE username = ?');
     return query.get(username);
 }
 
 // add a new user to the db
 export const newUser = (username, displayName, personalEmail) => {
-    console.log(username, displayName, personalEmail)
     const query = db.prepare('INSERT INTO Users (username, displayName, personalEmail) VALUES (?, ?, ?)');
     return query.run(username, displayName, personalEmail);
+}
+
+// give a user a type
+export const specUserType = (userID, userType) => {
+    const query = db.prepare('INSERT INTO userTypes VALUES (?, ?)');
+    return query.run(userID, userType);
 }
