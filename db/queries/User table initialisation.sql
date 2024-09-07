ac6efea886c65b9216ffbca6701c7cde80732448
Matthew Townson <mjwtownson@gmail.com>
2024-09-07 19:24:50 +0100

    added creation for usertypes

--- db/queries/User table initialisation.sql
+++ db/queries/User table initialisation.sql
@@ -1,12 +1,19 @@
 -- SQLite
 create table
   `Users` (
-    `UserID` integer not null primary key autoincrement,
+    `userID` integer not null primary key autoincrement,
     `username` varchar(64) not null,
     `displayName` varchar(64) not null,
     `personalEmail` varchar(255) not null,
     `webEmail` varchar(255) null,
-    `userType` TINYINT not null default 6,
     `membershipEnd` datetime null,
     unique (`UserID`)
-  )
+  );
+
+create table
+  `UserTypes` (
+    `userID` integer not null,
+    `userType` TINYINT not null,
+    primary key (`userID`, `userType`),
+    foreign key (`userID`) references Users (`userID`)
+  );
